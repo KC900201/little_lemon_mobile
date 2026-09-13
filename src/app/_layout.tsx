82b2e4fdea96d7b92c4e85efafa4d5e9fba86c11
KC Ng <nkc900201@gmail.com>
2026-09-13 20:39:55 +0800

    feat: (WIP) setup style configuration, add in design and UI kit

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,9 +1,23 @@
+import { Karla_400Regular, Karla_500Medium, Karla_700Bold } from "@expo-google-fonts/karla"
+import { MarkaziText_400Regular, MarkaziText_500Medium, useFonts } from "@expo-google-fonts/markazi-text"
 import { QueryClientProvider } from "@tanstack/react-query"
 import { Stack } from "expo-router"
 
 import { queryClient } from "@/shared/lib/query-client"
 
+import "@/global.css"
+
 export default function RootLayout() {
+  const [fontsLoaded] = useFonts({
+    MarkaziText: MarkaziText_400Regular,
+    "MarkaziText-Medium": MarkaziText_500Medium,
+    Karla: Karla_400Regular,
+    "Karla-Medium": Karla_500Medium,
+    "Karla-Bold": Karla_700Bold
+  })
+
+  if(!fontsLoaded) return null
+
   return (
     <QueryClientProvider client={queryClient}>
       <Stack />
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,36 @@
-
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./App.{js,jsx,ts,tsx}", "./src/**/*.{js,jsx,ts,tsx}", "./app/**/*.{js,jsx,ts,tsx}"],
-  presets: [require("nativewind/preset")],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-};
+	content: ["./App.{js,jsx,ts,tsx}", "./src/**/*.{js,jsx,ts,tsx}", "./app/**/*.{js,jsx,ts,tsx}"],
+	presets: [require("nativewind/preset")],
+	theme: {
+		extend: {
+			colors: {
+				primary: {
+					green: "#495E57",
+					yellow: "#F4CE14",
+				},
+				accent: {
+					green: "#48742C",
+				},
+				placeholder: "#D9D9D9",
+				secondary: {
+					salmon: "#EE9972",
+					peach: "#FBDABB",
+					gray: "#EDEFEE",
+					dark: "#333333",
+				},
+			},
+			fontFamily: {
+				display: ["MarkaziText"],
+				body: ["Karla"],
+				"body-medium": ["Karla-Medium"],
+				"body-bold": ["Karla-Bold"],
+			},
+			borderRadius: {
+				pill: "16px",
+				card: "8px",
+			},
+		},
+	},
+	plugins: [],
+}
